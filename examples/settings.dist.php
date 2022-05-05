ce6c995f9dff8309fe7506e043a076326b73ed84
Jan Slabon <jan.slabon@setasign.com>
2022-05-05 17:26:58 +0200

    Update settings.dist.php

--- examples/settings.dist.php
+++ examples/settings.dist.php
@@ -9,7 +9,7 @@ return [
     // Set to a string to specify the path to a file containing a PEM formatted client side certificate.
     // If a password is required, then set to an array containing the path to the PEM file in the first array
     // element followed by the password required for the certificate in the second array element.
-    'cert' => realpath(__DIR__ . '/private/publickey.pem'),
+    'cert' => realpath(__DIR__ . '/private/mTLS-Cert.pem'),
     // Specify the path to a file containing a private SSL key in PEM format.
     // If a password is required, then set to an array containing the path to the SSL key in the first array element
     // followed by the password required for the certificate in the second element.
